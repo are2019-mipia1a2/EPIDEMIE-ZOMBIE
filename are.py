bd36a62c1b548cef7d8ea0736f7a404e9591938c
astridricard <47814007+astridricard@users.noreply.github.com>
2019-03-13 09:50:43 +0100

    Update are.py

--- are.py
+++ are.py
@@ -7,4 +7,13 @@ from Parametres import *
 
 ##### HUMAIN
 
+def liste_tuple(liste):
+  return [(k.place_MATRICE) for k in liste]
+
+class Humain:
+  ''' classe définissant un humain pouvant se déplacer et fuir les zomnies'''
+  
+  num_humain=0
+  
+  
 
