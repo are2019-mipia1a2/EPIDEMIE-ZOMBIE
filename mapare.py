da22165c413e3d7c902f3ffff4bf05851e241554
astridricard <47814007+astridricard@users.noreply.github.com>
2019-03-20 11:58:05 +0100

    Update mapare.py

--- mapare.py
+++ mapare.py
@@ -1,21 +1,6 @@
 
 
-import matplotlip.animation as animation
-from Parametres import*
-from Classe import*
-from Environement import*
-
-evolution_H = [] #liste du nombre d'Humains à chaque tour
-evolution_Z = [] #liste du nombre de Zombies à chaque tour
-
-
-Liste_humain
-Liste_zombie
-Liste_porte
-Liste_batiment
-Liste_hopital
-taillex=i
-tailley=j
+      
 
 
 
