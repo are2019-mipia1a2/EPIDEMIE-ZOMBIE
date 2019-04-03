8b83eafae5f235f97ce83fff595c362e2a474799
astridricard <47814007+astridricard@users.noreply.github.com>
2019-04-03 10:45:41 +0200

    Update mapare.py

--- mapare.py
+++ mapare.py
@@ -1,4 +1,13 @@
-
+def set_map(carte):
+   """
+   list[list[int*Object]] -> liste[list[Object]]
+   Remplace les zéros de la matrice initiale par la classe Vide
+   """
+   for ligne in range (i):
+       for colonne in range(j):
+           if carte[ligne][colonne] == 0:
+               carte[ligne][colonne] = Vide()
+   return carte
 
       
 
